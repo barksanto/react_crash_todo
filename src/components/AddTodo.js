21e6a01ecc84ed368a1e33eae3d891b11408243b
Barkley Santo <barkley.santo@gmail.com>
2021-01-12 16:27:08 -0600

    added state to AddTodo component - and method started to be able to create title for the new todo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+  state = {
+    title: ''
+  }
+
+  //the state is in THIS COMPONENT thats why we can do this fn here
+  //setting the title to whatever we type in
+  onChange = (e) => this.setState({ title: e.target.value });
+
   render() {
     return (
       <form style={{ display: 'flex' }}>
@@ -8,7 +16,9 @@ export class AddTodo extends Component {
           style={{ flex: '10', padding: '5px' }}
           type="text"
           name="title"
-          placeholder="Add Todo..." />
+          placeholder="Add Todo..."
+          value={this.state.title}
+          onChange={this.onChange} />
         <input
           type="submit"
           value="Submit"
