e2094ba201a981ec022a46dc8c5b5e01e8c93ed2
Barkley Santo <barkley.santo@gmail.com>
2021-01-12 14:52:50 -0600

    added style for each todo - conditional styling for textDecoration for line through depending if completeted prop coming form state is true or not

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export class TodoItem extends Component {
+
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+  }
+
   render() {
     return (
       // inline style uses double curlybraces {{ backgroundColor: '#f4f4f4' }}
-      // but in this case we use a variable - which only needs one pair of braces
-      <div style={itemStyle}>
+      // but in this case we use a variable - which only needs one pair of braces {itemStyle}
+      <div style={this.getStyle()}>
         <p>{this.props.todo.title}</p>
       </div>
     )
@@ -18,7 +28,7 @@ TodoItem.propTypes = {
   todos: PropTypes.object.isRequired
 }
 
-const itemStyle = {
-  backgroundColor: '#f4f4f4'
-}
+// const itemStyle = {
+//   backgroundColor: '#f4f4f4'
+// }
 export default TodoItem
