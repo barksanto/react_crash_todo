f376ce8df4482d04482cbcd25ec6eef0ed8171c0
Barkley Santo <barkley.santo@gmail.com>
2021-01-12 14:44:06 -0600

    inline style and using a variable that carries the style like itemStyle

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types'
 export class TodoItem extends Component {
   render() {
     return (
-      <div>
+      // inline style uses double curlybraces {{ backgroundColor: '#f4f4f4' }}
+      // but in this case we use a variable - which only needs one pair of braces
+      <div style={itemStyle}>
         <p>{this.props.todo.title}</p>
       </div>
     )
@@ -16,5 +18,7 @@ TodoItem.propTypes = {
   todos: PropTypes.object.isRequired
 }
 
-
+const itemStyle = {
+  backgroundColor: '#f4f4f4'
+}
 export default TodoItem
